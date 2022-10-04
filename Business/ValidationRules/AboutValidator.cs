76fb4d1b2b9860f931abe53cd7699c736ccd7a91
Gokhan <engokhangok@gmail.com>
2022-10-05 00:34:05 +0300

    Added Fluent Validation

---
+++ Business/ValidationRules/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description cannot be empty ...!");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Please select an image");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Write a description of at least 50 characters");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Please shorten the description");
+        }
+    }
+}
