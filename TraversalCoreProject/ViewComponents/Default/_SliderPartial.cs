ddfba05f8af5f7f45867d49fb8c69b5d44f0a7fc
Gokhan <engokhangok@gmail.com>
2022-10-06 03:25:38 +0300

    Added Default Index and View Components.

---
+++ TraversalCoreProject/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _SliderPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }
+    }
+}
