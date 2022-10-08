6064f19cf742cc41ca4a977321dcc8ac2ed7a0e0
Gokhan <engokhangok@gmail.com>
2022-10-08 16:30:27 +0300

    Added Listing of Tour Routes.

---
+++ Business/Concrete/DestinationManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+        IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+
+        public Destination GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Destination> TGetList()
+        {
+           return _destinationDal.GetList();
+        }
+
+        public void TUpdate(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke() 
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
