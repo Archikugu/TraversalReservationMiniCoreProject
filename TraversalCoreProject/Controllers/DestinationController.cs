11cca412d4925496e8bed0addff4dc9a36cdaf95
Gokhan <engokhangok@gmail.com>
2022-10-11 23:48:03 +0300

    Added Layout Edits and Single Page Page.

---
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id) 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
+        }
+    }
+}
