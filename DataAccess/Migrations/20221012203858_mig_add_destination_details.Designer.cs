5b1dfce1817479ff46b5738862d84da8d60abbfe
Gokhan <engokhangok@gmail.com>
2022-10-12 23:41:50 +0300

    Database Updated.

--- DataAccess/Migrations/20221012203858_mig_add_destination_details.Designer.cs
+++ DataAccess/Migrations/20221012203858_mig_add_destination_details.Designer.cs
@@ -3,14 +3,16 @@ using DataAccess.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20221012203858_mig_add_destination_details")]
+    partial class mig_add_destination_details
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Entity/Concrete/Destination.cs
+++ Entity/Concrete/Destination.cs
@@ -15,9 +15,13 @@ namespace Entity.Concrete
         public string DayNight { get; set; }
         public double Price { get; set; }
         public string Image { get; set; }
+        public string Image2 { get; set; }
         public string Description { get; set; }
         public int Capacity { get; set; }
         public bool Status { get; set; }
+        public string CoverImage { get; set; }
+        public string Details1 { get; set; }
+        public string Details2 { get; set; }
 
 
     }
