506de66ac61be24b39db1b1623361f5f605260e3
Gokhan <engokhangok@gmail.com>
2022-10-22 18:31:25 +0300

    Added Login and Resigter Pages.

---
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        [AllowAnonymous]
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignUp() 
+        //{
+        //    return View();
+        //}
+        
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
