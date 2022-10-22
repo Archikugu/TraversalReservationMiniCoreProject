0348f13168eb6b799c983233b02a9594b749ebcf
Gokhan <engokhangok@gmail.com>
2022-10-22 17:28:44 +0300

    Added Identity Library.

--- DataAccess/Concrete/Context.cs
+++ DataAccess/Concrete/Context.cs
@@ -1,4 +1,5 @@
 ﻿using Entity.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete
 {
-    public class Context:DbContext
+    public class Context:IdentityDbContext<AppUser,AppRole,int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
         {
