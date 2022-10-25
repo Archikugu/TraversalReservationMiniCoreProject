50d02fb16e77c86a25a03c08bdb625642240fc19
Gokhan <engokhangok@gmail.com>
2022-10-25 17:48:35 +0300

    Added Data Listing on Member Page.

---
+++ TraversalCoreProject/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
