8240a8954119b406b4086e87f05d380b095c7023
Gokhan <engokhangok@gmail.com>
2022-11-03 10:15:35 +0300

    Added Reservation Page.

---
+++ Business/Concrete/ReservationManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ReservationManager : IReservationService
+    {
+        IReservationDal _reservationDal;
+
+        public ReservationManager(IReservationDal reservationDal)
+        {
+            _reservationDal = reservationDal;
+        }
+
+        public void TAdd(Reservation t)
+        {
+            _reservationDal.Insert(t);
+        }
+
+        public void TDelete(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
@@ -1,11 +1,20 @@
-﻿using Entity.Concrete;
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TraversalCoreProject.Areas.Member.Controllers
 {
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
+
+
         public IActionResult MyCurrentReservation()
         {
             return View();
@@ -14,14 +23,24 @@ namespace TraversalCoreProject.Areas.Member.Controllers
         {
             return View();
         }
+        [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text=x.City,
+                                               Value=x.DestinationId.ToString(),
+                                           }).ToList();
+            ViewBag.v=values;
             return View();
         }
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserId = 5;
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
         }
     }
 }
