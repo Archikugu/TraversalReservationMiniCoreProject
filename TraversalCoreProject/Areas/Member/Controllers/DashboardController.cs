4eb74cdf10026d5d85e7647c213d801b99e8d190
Gokhan <engokhangok@gmail.com>
2022-11-09 23:44:02 +0300

    Added Member Profile Dashboard.

---
+++ TraversalCoreProject/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.SurName;
+            ViewBag.userImage = values.ImageUrl;
+            return View();
+        }
+    }
+}
