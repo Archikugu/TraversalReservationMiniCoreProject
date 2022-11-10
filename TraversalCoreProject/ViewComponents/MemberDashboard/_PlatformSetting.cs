d2031f6dc4f28baaec8f69b04a20060aa05dfbe8
Gokhan <engokhangok@gmail.com>
2022-11-10 17:29:07 +0300

    Added Platform Settings and View Component.

---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,13 @@
+﻿using Business.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _PlatformSetting:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { 
+            return View();
+        }
+    }
+}
