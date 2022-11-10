02581348e8420dba14db947bf47e8bc846453230
Gokhan <engokhangok@gmail.com>
2022-11-10 15:11:45 +0300

    Added Listing of Data in Dashboard.

---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.memberName = values.Name + " " + values.SurName;
+            ViewBag.memberPhone = values.PhoneNumber;
+            ViewBag.memberMail = values.Email;
+
+            return View();
+        }
+    }
+}
