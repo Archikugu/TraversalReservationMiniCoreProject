1b93d75da31c6e326139b0de4fb010b723a653c8
Gokhan <engokhangok@gmail.com>
2022-11-10 15:54:50 +0300

    Added Guides List.

---
+++ Business/Concrete/GuideManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class GuideManager : IGuideService
+    {
+        IGuideDal _guideDal;
+
+        public GuideManager(IGuideDal guideDal)
+        {
+            _guideDal = guideDal;
+        }
+
+        public void TAdd(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guide TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Guide> TGetList()
+        {
+            return _guideDal.GetList();
+        }
+
+        public void TUpdate(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _GuideList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            GuideManager guideManager = new GuideManager(new EfGuideDal());
+
+            var values = guideManager.TGetList();
+            return View(values);
+        }
+    }
+}
