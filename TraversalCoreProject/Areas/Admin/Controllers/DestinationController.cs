0eeb05831ee98df2540e371f61d19020d6d19411
Gokhan <engokhangok@gmail.com>
2022-11-15 14:00:32 +0300

    Added admin destination page.

--- TraversalCoreProject/Areas/Admin/Controllers/DestinationController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = destinationManager.TGetByID(id);
+            destinationManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateDestination(int id) 
+        {
+            var values = destinationManager.TGetByID(id);
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index");
         }
     }
 }
