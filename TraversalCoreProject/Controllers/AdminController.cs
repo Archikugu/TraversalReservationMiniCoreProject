c1ef0261e0c3198e01215d6edc41a4ef7fea8f82
Gokhan <engokhangok@gmail.com>
2022-11-16 15:21:11 +0300

    Added  Admin Layout Partial Views.

--- TraversalCoreProject/Controllers/AdminController.cs
+++ TraversalCoreProject/Controllers/AdminController.cs
@@ -12,5 +12,21 @@ namespace TraversalCoreProject.Controllers
         {
             return PartialView();
         }
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavBar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
