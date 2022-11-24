e4b964b528a73f39b93ae2ca4b5317b40b251c97
Gokhan <engokhangok@gmail.com>
2022-11-24 23:49:51 +0300

    Added Ajax Operations 1.

---
+++ TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+
+        public static List<CityClass> cities = new List<CityClass>
+        {
+            new CityClass ()
+            {
+                CityId= 1,
+                CityName="Paris",
+                CityCountry="France"
+            },
+            new CityClass ()
+            {
+                CityId= 2,
+                CityName="Roma",
+                CityCountry="Italy"
+            },new CityClass ()
+            {
+                CityId= 3,
+                CityName="London",
+                CityCountry="England"
+            },
+        };
+    }
+}
---
+++ TraversalCoreProject/Models/CityClass.cs
@@ -0,0 +1,9 @@
+﻿namespace TraversalCoreProject.Models
+{
+    public class CityClass
+    {
+        public int CityId{ get; set; }
+        public string CityName { get; set; }
+        public string CityCountry { get; set; }
+    }
+}
