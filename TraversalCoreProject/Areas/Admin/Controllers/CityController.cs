d7b761b48d05f4490cacfa5dc8aa6595854df446
Gokhan <engokhangok@gmail.com>
2022-11-25 12:58:23 +0300

    With ajax operations 2, city listing operations have been made dynamic.

--- TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using TraversalCoreProject.Models;
@@ -8,6 +10,13 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
     [Area("Admin")]
     public class CityController : Controller
     {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,29 +24,47 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
 
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
             return Json(jsonCity);
         }
 
-        public static List<CityClass> cities = new List<CityClass>
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
         {
-            new CityClass ()
-            {
-                CityId= 1,
-                CityName="Paris",
-                CityCountry="France"
-            },
-            new CityClass ()
-            {
-                CityId= 2,
-                CityName="Roma",
-                CityCountry="Italy"
-            },new CityClass ()
-            {
-                CityId= 3,
-                CityName="London",
-                CityCountry="England"
-            },
-        };
+            destination.Status = true;
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination); 
+            return Json(values);
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var values = _destinationService.TGetByID(id);
+            var jsonValues=JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+
+
+        //Static City Class
+        /*  public static List<CityClass> cities = new List<CityClass>
+          {
+              new CityClass ()
+              {
+                  CityId= 1,
+                  CityName="Paris",
+                  CityCountry="France"
+              },
+              new CityClass ()
+              {
+                  CityId= 2,
+                  CityName="Roma",
+                  CityCountry="Italy"
+              },new CityClass ()
+              {
+                  CityId= 3,
+                  CityName="London",
+                  CityCountry="England"
+              },
+          };*/
     }
 }
