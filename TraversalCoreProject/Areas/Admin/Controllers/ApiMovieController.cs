8775b264962767c17c832d6247fe7e40bd298825
Gokhan <engokhangok@gmail.com>
2022-12-15 23:44:18 +0300

    Added Rapid Api Imbd Top 100 Movie List

---
+++ TraversalCoreProject/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using DocumentFormat.OpenXml.Office2021.DocumentTasks;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TraversalCoreProject.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovies = new List<ApiMovieViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "5e9de8dc85msh8a73ebada00ada2p1e00cdjsne172b6e91af1" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovies = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovies);
+            }
+        }
+    }
+}
---
+++ TraversalCoreProject/Areas/Admin/Models/ApiMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TraversalCoreProject.Areas.Admin.Models
+{
+    public class ApiMovieViewModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+        public string rating { get; set; }
+        public int year { get; set; }
+        public string trailer { get; set; }
+    }
+}
