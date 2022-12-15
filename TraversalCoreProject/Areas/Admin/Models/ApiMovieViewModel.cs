79f60d35f116c473c90bd5902a4dc21c240b9191
Gokhan <engokhangok@gmail.com>
2022-12-16 01:03:49 +0300

    Edit Imbd Top 100 Movie List Page

--- TraversalCoreProject/Areas/Admin/Models/ApiMovieViewModel.cs
+++ TraversalCoreProject/Areas/Admin/Models/ApiMovieViewModel.cs
@@ -5,6 +5,7 @@
         public int rank { get; set; }
         public string title { get; set; }
         public string rating { get; set; }
+        public string image { get; set; }
         public int year { get; set; }
         public string trailer { get; set; }
     }
