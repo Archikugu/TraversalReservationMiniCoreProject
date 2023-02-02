d8bc74979edd90e4dd54c6a7ce85e544bddb1ffd
Gokhan <engokhangok@gmail.com>
2023-02-02 20:29:16 +0300

    Design fixes1

--- TraversalCoreProject/Controllers/DefaultController.cs
+++ TraversalCoreProject/Controllers/DefaultController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Controllers
 {
+    [AllowAnonymous]
     public class DefaultController : Controller
     {
         public IActionResult Index()
