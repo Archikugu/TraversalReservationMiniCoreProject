f20f9f86a2eeab1e2f5101bd9f91727ca4cc5811
Gokhan <engokhangok@gmail.com>
2023-02-24 11:48:36 +0300

    Added SignalR and PostgreSql database to the project

---
+++ SignalRApi/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRApi.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
---
+++ SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SignalRApi.DAL
+{
+    public enum ECity
+    {
+        Berlin=1,
+        İstanbul=2,
+        Ankara=3,
+        London=4,
+        Eskisehir=5
+    }
+    public class Visitor
+    {
+        public int VisitorId { get; set; }
+        public ECity City { get; set; }
+        public int CityVisitCount { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
