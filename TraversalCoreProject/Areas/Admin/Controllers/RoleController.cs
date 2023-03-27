3798f000b9549ae5c37bbea303e9c7137ebf9a46
Gokhan <engokhangok@gmail.com>
2023-03-27 18:44:51 +0300

    Added Role List and Add Roles.

---
+++ TraversalCoreProject/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,52 @@
+﻿using Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Role")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        [Route("CreateRole")]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateRole")]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
+        {
+            AppRole role = new AppRole()
+            {
+                Name = createRoleViewModel.RoleName
+            };
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ TraversalCoreProject/Areas/Admin/Models/CreateRoleViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TraversalCoreProject.Areas.Admin.Models
+{
+    public class CreateRoleViewModel
+    {
+        public string RoleName { get; set; }
+    }
+}
